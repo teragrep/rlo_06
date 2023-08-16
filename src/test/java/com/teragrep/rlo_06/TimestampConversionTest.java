c4be42e599113f30a830b86a1493c4663d967ca2
StrongestNumber9 <16169054+StrongestNumber9@users.noreply.github.com>
2023-08-16 17:34:47 +0300

    Adds a note about missing seconds being a feature

--- src/test/java/com/teragrep/rlo_06/TimestampConversionTest.java
+++ src/test/java/com/teragrep/rlo_06/TimestampConversionTest.java
@@ -17,6 +17,9 @@ public class TimestampConversionTest {
         Assertions.assertEquals("1985-04-12T19:20:50.520-04:00", getTimestamp("1985-04-12T19:20:50.52-04:00"));
         Assertions.assertEquals("2003-10-11T22:14:15.003Z", getTimestamp("2003-10-11T22:14:15.003Z"));
         Assertions.assertEquals("2023-08-16T17:09:00.123456+03:00", getTimestamp("2023-08-16T17:09:00.123456+03:00"));
+        // Missing seconds is a feature:
+        // "The format used will be the shortest that outputs the full value of the time where the omitted parts are implied to be zero."
+        // Source: https://docs.oracle.com/javase/8/docs/api/java/time/LocalDateTime.html#toString--
         Assertions.assertEquals("2023-01-01T00:00Z", getTimestamp("2023-01-01T00:00:00.000000+00:00"));
         Assertions.assertEquals("2023-01-01T00:00Z", getTimestamp("2023-01-01T00:00:00.000000-00:00"));
         Assertions.assertEquals("2023-01-01T00:00+02:00", getTimestamp("2023-01-01T00:00:00+02:00"));
