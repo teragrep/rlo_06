0183c84ed8958ff1ba57933d8a0c8419574eb25e
Motoko Kusanagi <kordex@gmail.com>
2024-09-12 10:50:08 +0300

    add PriorityCloseClock

--- src/test/java/com/teragrep/new_rlo_06/clocks/PriorityCloseClockTest.java
+++ src/test/java/com/teragrep/new_rlo_06/clocks/PriorityCloseClockTest.java
@@ -1,5 +1,85 @@
 package com.teragrep.new_rlo_06.clocks;
 
+import com.teragrep.new_rlo_06.ElementImpl;
+import com.teragrep.new_rlo_06.PriorityParseException;
+import com.teragrep.new_rlo_06.inputs.StringInput;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.nio.ByteBuffer;
+import java.util.ArrayList;
+import java.util.List;
+
 public class PriorityCloseClockTest {
-    // TODO
+    @Test
+    public void testCloseClock() {
+        StringInput input = new StringInput(">");
+        ByteBuffer[] buffers = input.asBuffers(1);
+
+        PriorityCloseClock clock = new PriorityCloseClock();
+
+        Assertions.assertFalse(clock.isComplete());
+
+        ByteBuffer out0 = clock.apply(buffers[0]);
+        Assertions.assertFalse(out0.hasRemaining());
+
+        Assertions.assertEquals(">", new ElementImpl(clock.get()).toString());
+
+        Assertions.assertTrue(clock.isComplete());
+    }
+
+    @Test
+    public void testCloseClockNoConsumeMore() {
+        StringInput input = new StringInput(">1");
+        ByteBuffer[] buffers = input.asBuffers(1);
+
+        PriorityCloseClock clock = new PriorityCloseClock();
+        ByteBuffer out0 = clock.apply(buffers[0]);
+        Assertions.assertTrue(out0.hasRemaining());
+
+        Assertions.assertTrue(clock.isComplete());
+
+        Assertions.assertEquals(">", new ElementImpl(clock.get()).toString());
+
+        ByteBuffer in1 = out0.slice();
+        List<ByteBuffer> ins = new ArrayList<>();
+        ins.add(in1);
+        Assertions.assertEquals("1", new ElementImpl(ins).toString());
+    }
+
+    @Test
+    public void testCloseClockNoConsumeMoreTwoBuffers() {
+        StringInput input = new StringInput(">1");
+        ByteBuffer[] buffers = input.asBuffers(2);
+
+        PriorityCloseClock clock = new PriorityCloseClock();
+
+        ByteBuffer out0 = clock.apply(buffers[0]);
+        Assertions.assertFalse(out0.hasRemaining());
+        Assertions.assertTrue(clock.isComplete());
+
+        Assertions.assertEquals(">", new ElementImpl(clock.get()).toString());
+
+        ByteBuffer out1 = clock.apply(buffers[1]);
+        Assertions.assertTrue(out1.hasRemaining());
+
+        ByteBuffer in1 = out1.slice();
+        List<ByteBuffer> ins = new ArrayList<>();
+        ins.add(in1);
+        Assertions.assertEquals("1", new ElementImpl(ins).toString());
+    }
+
+    @Test
+    public void testInvalidThrows() {
+        StringInput input = new StringInput("1");
+        ByteBuffer[] buffers = input.asBuffers(1);
+
+        PriorityCloseClock clock = new PriorityCloseClock();
+
+        PriorityParseException exception = Assertions.assertThrows(PriorityParseException.class, () -> {
+            clock.apply(buffers[0]);
+        });
+        Assertions.assertEquals("priority must end with a '>'", exception.getMessage());
+
+    }
 }
